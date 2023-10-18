4e89518b7229e56dae3266d7aee2c3b04b7b18df
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-10-18 19:47:02 -0400

    New named_pipes

--- TypeCPipes/TypeCPractical/CPPCommon/pipe_server.cpp
+++ TypeCPipes/TypeCPractical/CPPCommon/pipe_server.cpp
@@ -1,13 +1,76 @@
 #include <pipe_server.h>
 #include <exception>
 #include <system_error>
+#include <stdexcept>
 
 using std::vector;
 using std::string;
 using std::exception;
+using std::logic_error;
 using std::system_error;
 using std::error_code;
 using std::generic_category;
+
+named_pipes::pipe_server::pipe_server(string pipe_name)
+	: _pipe_handle(NULL), _pipe_name(pipe_name), _connected(false), _created(false)
+{
+}
+
+void named_pipes::pipe_server::create(string pipe_name)
+{
+	if (_created)
+		throw logic_error("Pipe was already created.");
+	if (pipe_name.length())
+		_pipe_name = pipe_name;
+
+	_created = true;
+}
+
+void named_pipes::pipe_server::send(void* buffer, size_t length)
+{
+	if (!_connected)
+		throw logic_error("Pipe is not connected.");
+}
+
+void named_pipes::pipe_server::send(const char* const c_str)
+{
+	send((void*)c_str, strnlen_s(c_str, 255));
+}
+
+void named_pipes::pipe_server::send(string str)
+{
+	send((void*)str.c_str(), str.length());
+}
+
+void named_pipes::pipe_server::connect()
+{
+	if (!_created)
+		throw logic_error("Pipe was not created.");
+	if (_connected)
+		throw logic_error("Pipe was already connected");
+
+	_connected = true;
+}
+
+void named_pipes::pipe_server::disconnect()
+{
+	if (!_connected)
+		throw logic_error("Pipe was already disconnected.");
+}
+
+void named_pipes::pipe_server::destroy()
+{
+	_created = false;
+}
+
+HANDLE named_pipes::pipe_server::handle()
+{
+	return _pipe_handle;
+}
+
+named_pipes::pipe_server::~pipe_server()
+{
+}
 
 pipe_server::pipe_server(string pipe_name)
 	: pipe_handle(INVALID_HANDLE_VALUE), pipe_name("\\\\.\\pipe\\" + pipe_name), _connected(false)
--- TypeCPipes/TypeCPractical/CPPCommon/pipe_server.h
+++ TypeCPipes/TypeCPractical/CPPCommon/pipe_server.h
@@ -2,6 +2,42 @@
 #include <string>
 #include <Windows.h>
 #include <vector>
+#include <iterator>
+
+namespace named_pipes
+{
+	class pipe_server
+	{
+	public:
+		pipe_server(std::string pipe_name);
+
+		void create(std::string pipe_name = std::string());
+
+		void send(void* buffer, size_t length);
+
+		void send(const char* const c_str);
+
+		void send(std::string str);
+
+		void connect();
+
+		void disconnect();
+
+		void destroy();
+
+		HANDLE handle();
+
+		~pipe_server();
+	private:
+		HANDLE _pipe_handle;
+
+		std::string _pipe_name;
+
+		bool _connected;
+
+		bool _created;
+	};
+}
 
 struct pipe_server
 {
